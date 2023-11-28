e77450973ccf389ca6854c49ed17360cb4397702
s-013-29 <s-013-29@gumiforest.ae.krakow.pl>
2023-11-28 11:16:05 +0100

    zajecia, poprawic wszystko

---
+++ 07-AggregationAndComposition/Writer.java
@@ -0,0 +1,16 @@
+public class Writer {
+    private String surname;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String displaySurname() {
+        return surname;
+    }
+
+}
